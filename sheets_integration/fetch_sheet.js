04f61b4e0f205f9208a55bf8840d442c67e5f2eb
Zynh <zynh0722@gmail.com>
2022-06-03 14:29:43 -0700

    Fixing some linting

--- sheets_integration/fetch_sheet.js
+++ sheets_integration/fetch_sheet.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const readline = require('readline');
-const {google} = require('googleapis');
+const { google } = require('googleapis');
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
@@ -10,13 +10,13 @@ const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 const TOKEN_PATH = 'token.json';
 
 function auth_and_read() {
-    // Load client secrets from a local file.
-    fs.readFile('credentials.json', (err, content) => {
-    if (err) return console.log('Error loading client secret file:', err);
-    // Authorize a client with credentials, then call the Google Sheets API.
-    console.log(JSON.parse(content))
-    authorize(JSON.parse(content), listMajors);
-    });
+	// Load client secrets from a local file.
+	fs.readFile('sheets_integration/credentials.json', (err, content) => {
+		if (err) return console.log('Error loading client secret file:', err);
+		// Authorize a client with credentials, then call the Google Sheets API.
+		console.log(JSON.parse(content));
+		authorize(JSON.parse(content), listMajors);
+	});
 }
 
 /**
@@ -26,16 +26,16 @@ function auth_and_read() {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
+	const { client_secret, client_id, redirect_uris } = credentials.installed;
+	const oAuth2Client = new google.auth.OAuth2(
+		client_id, client_secret, redirect_uris[0]);
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
+	// Check if we have previously stored a token.
+	fs.readFile(TOKEN_PATH, (err, token) => {
+		if (err) return getNewToken(oAuth2Client, callback);
+		oAuth2Client.setCredentials(JSON.parse(token));
+		callback(oAuth2Client);
+	});
 }
 
 /**
@@ -45,28 +45,28 @@ function authorize(credentials, callback) {
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
 function getNewToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error while trying to retrieve access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
-  });
+	const authUrl = oAuth2Client.generateAuthUrl({
+		access_type: 'offline',
+		scope: SCOPES,
+	});
+	console.log('Authorize this app by visiting this url:', authUrl);
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+	rl.question('Enter the code from that page here: ', (code) => {
+		rl.close();
+		oAuth2Client.getToken(code, (err, token) => {
+			if (err) return console.error('Error while trying to retrieve access token', err);
+			oAuth2Client.setCredentials(token);
+			// Store the token to disk for later program executions
+			fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+				if (err) return console.error(err);
+				console.log('Token stored to', TOKEN_PATH);
+			});
+			callback(oAuth2Client);
+		});
+	});
 }
 
 /**
@@ -75,28 +75,29 @@ function getNewToken(oAuth2Client, callback) {
  * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
  */
 function listMajors(auth) {
-  const sheets = google.sheets({version: 'v4', auth});
-  sheets.spreadsheets.values.get({
-    spreadsheetId: '17JmKV_TeXaaCulegDvaLdOhmBYWPcOJa7uKcV6YOarE',
-    range: 'A1:E2',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
+	const sheets = google.sheets({ version: 'v4', auth });
+	sheets.spreadsheets.values.get({
+		spreadsheetId: '17JmKV_TeXaaCulegDvaLdOhmBYWPcOJa7uKcV6YOarE',
+		range: 'A1:E2',
+	}, (err, res) => {
+		if (err) return console.log('The API returned an error: ' + err);
 
-    const rows = res.data.values;
-    if (rows.length) {
+		const rows = res.data.values;
+		if (rows.length) {
 
-      rows.map((row) => {
-        row.map((col) => {
-            process.stdout.write(col + ' ');
-        })
-        process.stdout.write('\n');
-        // console.log(`${row[0]}, ${row[4]}`);
-      });
-    } else {
-      console.log('No data found.');
-    }
+			rows.map((row) => {
+				row.map((col) => {
+					process.stdout.write(col + ' ');
+				});
+				process.stdout.write('\n');
+				// console.log(`${row[0]}, ${row[4]}`);
+			});
+		}
+		else {
+			console.log('No data found.');
+		}
 
-  });
+	});
 }
 
 module.exports.auth_and_read = auth_and_read;

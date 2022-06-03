f936d2522ec69b3ae930600d9d0643b7eeaad57c
Zynh <zynh0722@gmail.com>
2022-06-03 14:49:02 -0700

    More eslint adjustments

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ require('dotenv').config();
 // const sheetsHandler = require('./sheets_integration/fetch_sheet.js');
 
 // sheetsHandler.auth_and_read();
+
+const { Client, Intents } = require('discord.js');
+const token = process.env.BOT_TOKEN;
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// Run code when client is ready
+client.once('ready', () => {
+    console.log('Ready!');
+});

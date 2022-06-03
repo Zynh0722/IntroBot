03a9e7764d7b92c33eece6c23d63152894b46656
Zynh <zynh0722@gmail.com>
2022-06-03 14:58:39 -0700

    adding stuff

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
+const { Client, Intents } = require('discord.js');
+
 require('dotenv').config();
 
-// const sheetsHandler = require('./sheets_integration/fetch_sheet.js');
-
-// sheetsHandler.auth_and_read();
-
-const { Client, Intents } = require('discord.js');
-const token = process.env.BOT_TOKEN;
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
@@ -13,3 +9,5 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.once('ready', () => {
     console.log('Ready!');
 });
+
+client.login(process.env.BOT_TOKEN);

ec53b109477c40dce206769b604788bbb84ffbc4
Zynh <zynh0722@gmail.com>
2022-06-03 15:10:33 -0700

    Basic commands

---
+++ commands/command-handlers.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'ping': interaction => interaction.reply('Pong!'),
+    'server': interaction => interaction.reply('Server info!'),
+    'user': interaction => interaction.reply('User info.'),
+};
--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const { Client, Intents } = require('discord.js');
+const command_handlers = require('./commands/command-handlers');
+
+console.log(command_handlers);
 
 require('dotenv').config();
 
-
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    await command_handlers[commandName](interaction);
+});
 
 // Run code when client is ready
 client.once('ready', () => {

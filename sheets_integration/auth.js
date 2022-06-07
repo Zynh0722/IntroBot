0dc5d59c999c674fa8df195304be509febc66787
Zynh <zynh0722@gmail.com>
2022-06-07 14:47:53 -0700

    FULLY PROMISED!!!

--- sheets_integration/auth.js
+++ sheets_integration/auth.js
@@ -15,7 +15,8 @@ const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 function load_credentials() {
     return fs.readFileAsync(CRED_PATH)
         .then(content => JSON.parse(content))
-        .then(content => { authorize(content, listMajors); })
+        .then(content => authorize(content))
+        .then(auth => listMajors(auth))
         .catch(err => console.log('Error loading client secret file:', err));
 }
 
@@ -30,18 +31,16 @@ function setCredentials(oAuth2Client, token) {
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-function authorize(credentials, callback) {
+function authorize(credentials) {
     const { client_secret, client_id, redirect_uris } = credentials.installed;
     const oAuth2Client = new google.auth.OAuth2(
         client_id, client_secret, redirect_uris[0]);
 
     // Check if we have previously stored a token.
-    fs.readFileAsync(TOKEN_PATH)
+    return fs.readFileAsync(TOKEN_PATH)
         .then(token => JSON.parse(token))
-        .then(token => {
-            callback(setCredentials(oAuth2Client, token));
-        })
-        .catch(() => getNewToken(oAuth2Client, callback));
+        .then(token => setCredentials(oAuth2Client, token))
+        .catch(() => getNewToken(oAuth2Client));
 }
 
 /**
@@ -50,13 +49,13 @@ function authorize(credentials, callback) {
  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
-function getNewToken(oAuth2Client, callback) {
-
+function getNewToken(oAuth2Client) {
     const authUrl = oAuth2Client.generateAuthUrl({
         access_type: 'offline',
         scope: SCOPES,
     });
     console.log('Authorize this app by visiting this url:', authUrl);
+
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout,
@@ -66,13 +65,13 @@ function getNewToken(oAuth2Client, callback) {
     const getToken = Promise.promisify(oAuth2Client.getToken.bind(oAuth2Client));
     const question = (q) => new Promise(resolve => rl.question(q, resolve));
 
-    question('Enter the code from that page here: ')
+    return question('Enter the code from that page here: ')
         .tap(() => rl.close())
         .then(code => getToken(code))
         .tap(token => fs.writeFileAsync(TOKEN_PATH, JSON.stringify(token))
             .then(() => console.log('Token stored to', TOKEN_PATH))
             .catch(console.error))
-        .then(token => callback(setCredentials(oAuth2Client, token)))
+        .then(token => setCredentials(oAuth2Client, token))
         .catch(err => console.error('Error while trying to retrieve access token', err));
 }
 
